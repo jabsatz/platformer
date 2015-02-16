b923e9c065d27f39bd0bbbacbe4fff6a77aebf86
Julian Absatz <julianabsatz@gmail.com>
2015-02-16 20:40:22 -0300

    debuging info on screen

--- classes/engine.js
+++ classes/engine.js
@@ -9,11 +9,14 @@ function Engine(stage,level,player){
   this.crosshair = new Crosshair(0, 0);
   this.x         = 0;
   this.y         = 0;
+  this.debug    = new createjs.Text();
+  this.debug.x  = 800;
 };
 
 Engine.prototype.update = function(){
   this.player.update(this.level);
   this.crosshair.update(this.stage);
+  this.debug.text = this.player.x + ',' + Math.round(this.player.y) + ',' + Math.round(this.player.vely) + ',' + this.player.grounded;
 };
 
 Engine.prototype.draw = function() {
@@ -24,6 +27,8 @@ Engine.prototype.draw = function() {
   this.stage.addChild(this.crosshair.shape);
 
   this.stage.addChild(this.player.shape);
+
+  this.stage.addChild(this.debug);
 
   this.stage.update();
 };
